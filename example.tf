55193e8252eb1b5e86fba1342194d7392a189121
trey <trey@fehuit.com>
2018-08-30 16:58:45 -0500

    added key pairs

--- example.tf
+++ example.tf
@@ -2,6 +2,9 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
+variable "public_key_path" {}
+variable "aws_key_name" {}
+
 
 
 provider "aws" {
@@ -10,17 +13,31 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_instance" "TFexample" {
-  ami           = "ami-0cf31d971a3ca20d6"
-  instance_type = "t2.micro"
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.TFexample.public_ip} > ip_address.txt"
-    }
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.aws_key_name}"
+  public_key= "${file(var.public_key_path)}"
 }
 
 
+resource "aws_instance" "web" {
+  ami = "ami-0b59bfac6be064b78"
+  key_name = "${aws_key_pair.auth.key_name}"
+  instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.web.public_ip} > ip_address.txt"
+    }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo yum -y install httpd",
+      "sudo yum -y install mysql",
+      "sudo service httpd start",
+    ]
+  }
+}
+
 resource "aws_eip" "ip" {
-  instance = "${aws_instance.TFexample.id}"
+  instance = "${aws_instance.web.id}"
 }
 
 
